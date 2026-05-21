235c8c6d709a0d236603ccc9107886cb193e024b
Poojitha <k.poojitha.956@gmail.com>
2026-05-21 15:54:16 +0530

    First calculator project

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Choose (+, -, *, /, ^): ")
+if operation == "+":
+    print("Result is: ", num1 + num2)
+elif operation == "-":
+    print("Result is: ", num1 - num2)
+elif operation == "*":
+    print("Result is: ", num1 * num2)
+elif operation == "/":
+    if num2 != 0:
+        print("Result is: ", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+elif operation == "^":
+    print("Result is: ", num1 ** num2)
+else:
+    print("Invalid operation")
