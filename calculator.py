64796fdff25f412163fe178d659d3412dada85b4
Poojitha <k.poojitha.956@gmail.com>
2026-05-21 16:57:34 +0530

    Added power and square root features

--- calculator.py
+++ calculator.py
@@ -1,18 +1,25 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-operation = input("Choose (+, -, *, /, ^): ")
-if operation == "+":
-    print("Result is: ", num1 + num2)
-elif operation == "-":
-    print("Result is: ", num1 - num2)
-elif operation == "*":
-    print("Result is: ", num1 * num2)
-elif operation == "/":
-    if num2 != 0:
-        print("Result is: ", num1 / num2)
-    else:
-        print("Cannot divide by zero")
-elif operation == "^":
-    print("Result is: ", num1 ** num2)
+import math
+operation = input("Choose (+, -, *, /, ^, sqrt): ")
+if operation == "sqrt":
+    num = float(input("Enter number: "))
+    print("Square root:", math.sqrt(num))
 else:
-    print("Invalid operation")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == "+":
+        print("Result is: ", num1 + num2)
+    elif operation == "-":
+        print("Result is: ", num1 - num2)
+    elif operation == "*":
+        print("Result is: ", num1 * num2)
+    elif operation == "/":
+        if num2 != 0:
+            print("Result is: ", num1 / num2)
+        else:
+            print("Cannot divide by zero")
+    elif operation == "^":
+        print("Result is: ", num1 ** num2)
+    else:
+        print("Invalid operation")
+
