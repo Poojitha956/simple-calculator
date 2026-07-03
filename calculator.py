5860b77ded5c2d132dc3e6c264f569affaab8678
Poojitha <k.poojitha.956@gmail.com>
2026-07-03 16:36:00 +0530

    Refactored calculator using functions

--- calculator.py
+++ calculator.py
@@ -1,48 +1,39 @@
 import math
 
 def addition():
-    numbers = input("Enter numbers separated by spaces: ")
-    numbers = list(map(float, numbers.split()))
+    numbers = list(map(float, input("Enter numbers separated by spaces: ").split()))
     print("Result is:", sum(numbers))
 
 def subtraction():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    print("Result is:", num1 - num2)
+    numbers = list(map(float, input("Enter two numbers separated by spaces: ").split()))
+    print("Result is:", numbers[0] - numbers[1])
 
 def multiplication():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-    print("Result is:", num1 * num2)
+    numbers = list(map(float, input("Enter two numbers separated by spaces: ").split()))
+    print("Result is:", numbers[0] * numbers[1])
 
 def division():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-
-    if num2 != 0:
-        print("Result is:", num1 / num2)
+    numbers = list(map(float, input("Enter two numbers separated by spaces: ").split()))
+    if numbers[1] != 0:
+        print("Result is:", numbers[0] / numbers[1])
     else:
         print("Cannot divide by zero")
 
 def power():
-    num1 = float(input("Enter base number: "))
-    num2 = float(input("Enter exponent: "))
-    print("Result is:", num1 ** num2)
+    numbers = list(map(float, input("Enter base and exponent separated by spaces: ").split()))
+    print("Result is:", numbers[0] ** numbers[1])
 
 def modulus():
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-
-    if num2 != 0:
-        print("Result is:", num1 % num2)
+    numbers = list(map(float, input("Enter two numbers separated by spaces: ").split()))
+    if numbers[1] != 0:
+        print("Result is:", numbers[0] % numbers[1])
     else:
         print("Cannot divide by zero")
 
 def square_root():
-    num = float(input("Enter number: "))
-
-    if num >= 0:
-        print("Square root:", math.sqrt(num))
+    numbers = list(map(float, input("Enter one number: ").split()))
+    if numbers[0] >= 0:
+        print("Square root:", math.sqrt(numbers[0]))
     else:
         print("Square root of a negative number is not possible.")
 
@@ -50,24 +41,17 @@ operation = input("Choose (+, -, *, /, ^, %, sqrt): ")
 
 if operation == "+":
     addition()
-
 elif operation == "-":
     subtraction()
-
 elif operation == "*":
     multiplication()
-
 elif operation == "/":
     division()
-
 elif operation == "^":
     power()
-
 elif operation == "%":
     modulus()
-
 elif operation == "sqrt":
     square_root()
-
 else:
     print("Invalid operation")
